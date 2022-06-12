cf616e3b53116eeec0f294d3e4e52d6564a73e6b
supun64 <l.w.supunmadusanka64@gmail.com>
2022-06-13 01:18:48 +0530

    AdminHome: Implemented Who is away

--- front-end/src/Pages/HomePage/index.jsx
+++ front-end/src/Pages/HomePage/index.jsx
@@ -54,8 +54,57 @@ function HomePage() {
   const work_home_data = [
     ["Working Type", "Number"],
     ["Home-Working", 52],
-    ["Physical-Working", 102]
-  ]
+    ["Physical-Working", 102],
+  ];
+
+  // Away people data
+  const today_absence = [
+    {
+      id: "123123K",
+      dp: "profile-pic-lona.jpg",
+      name: "Nethmi Jayakody",
+      post: "Admin",
+    },
+    {
+      id: "123443K",
+      dp: "profile-pic-cota.jpg",
+      name: "Nethmi Jayakody",
+      post: "Admin",
+    },
+    {
+      id: "111123K",
+      dp: "profile-pic-kj.png",
+      name: "Nethmi Jayakody",
+      post: "Admin",
+    },
+  ];
+
+  const tomorrow_absence = [
+    {
+      id: "435123K",
+      dp: "profile-pic-cota.jpg",
+      name: "Nethmi Jayakody",
+      post: "Admin",
+    },
+    {
+      id: "567123K",
+      dp: "profile-pic-lona.jpg",
+      name: "Nethmi Jayakody",
+      post: "Admin",
+    },
+    {
+      id: "789123K",
+      dp: "profile-pic-dedsec.jpg",
+      name: "Nethmi Jayakody",
+      post: "Admin",
+    },
+    {
+      id: "789133K",
+      dp: "profile-pic-kj.png",
+      name: "Nethmi Jayakody",
+      post: "Admin",
+    },
+  ];
 
   return (
     <div className={styled["page-holder"]}>
@@ -194,46 +243,82 @@ function HomePage() {
       </section>
 
       <section className={styled["overall-static-section"]}>
-        <section className={styled["graph-view"]}>
-          <div className={styled["stat-graph"]}>
-            {/* This is the pie chart for overall leaves */}
-            <Chart
-              chartType="PieChart"
-              data={leave_data}
-              options={{
-                title: "Today Leaves",
-                pieSliceTextStyle: {fontSize: 15},
-                colors: ["orange", "green", "#e32424", "black"],
-                legend: { position: "right", textStyle: { fontSize: 12 }},
-                tooltip: { trigger: "none" },
-                chartArea: {left: 25, width: "100%"}
-              }}
-              width={"100%"}
-              height={"100%"}
-            />
-          </div>
-          <div className={styled["stat-graph"]}>
+        <div className={styled["stat-graph"]}>
+          {/* This is the pie chart for overall leaves */}
           <Chart
-              chartType="PieChart"
-              data={work_home_data}
-              options={{
-                title: "Work From Home",
-                pieSliceTextStyle: {fontSize: 20},
-                colors: ["black", "#e32424"],
-                legend: { position: "right", textStyle: { fontSize: 12 }},
-                tooltip: { trigger: "none" },
-                chartArea: {left: 10, width: "100%"}
-              }}
-              width={"100%"}
-              height={"100%"}
-            />
-          </div>
-        </section>
+            chartType="PieChart"
+            data={leave_data}
+            options={{
+              title: "Today Leaves",
+              pieSliceTextStyle: { fontSize: 15 },
+              colors: ["orange", "green", "#e32424", "black"],
+              legend: { position: "right", textStyle: { fontSize: 12 } },
+              tooltip: { trigger: "none" },
+              chartArea: { left: 25, width: "100%" },
+            }}
+            width={"100%"}
+            height={"100%"}
+          />
+        </div>
+        <div className={styled["stat-graph"]}>
+          <Chart
+            chartType="PieChart"
+            data={work_home_data}
+            options={{
+              title: "Work From Home",
+              pieSliceTextStyle: { fontSize: 20 },
+              colors: ["black", "#e32424"],
+              legend: { position: "right", textStyle: { fontSize: 12 } },
+              tooltip: { trigger: "none" },
+              chartArea: { left: 10, width: "100%" },
+            }}
+            width={"100%"}
+            height={"100%"}
+          />
+        </div>
 
-        <section className={styled["absance-stats"]}>
-          <div className={styled["today"]}>Hello</div>
-          <div className={styled["tomorrow"]}></div>
-        </section>
+        <div className={styled["absance-stats"]}>
+          <h3>Who's Away</h3>
+          <div className={styled["date-stat"]}>
+            <h5>
+              <b>Today</b>
+            </h5>
+            <div className={styled["avatar-container"]}>
+              {today_absence.map((profile) => (
+                <div key={profile.id} className={styled["avatar"]}>
+                  <img
+                    className={styled["avatar-img"]}
+                    src={require("../../Images/ProfileAvatar/" + profile["dp"])}
+                    alt={profile["name"]}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className={styled["date-stat"]}>
+            <h5>
+              <b>Tomorrow</b>
+            </h5>
+            <div className={styled["avatar-container"]}>
+              {tomorrow_absence.map((profile, index) => {
+                if (index > 10) {
+                  return(<></>);
+                }
+
+                return (
+                  <div key={profile.id} className={styled["avatar"]}>
+                    <img
+                      className={styled["avatar-img"]}
+                      src={require("../../Images/ProfileAvatar/" +
+                        profile["dp"])}
+                      alt={profile["name"]}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   );
