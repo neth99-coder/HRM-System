4580f41aaf1cdc3f158d42565718cb8da41a7b49
poornaSavindi <89610997+poornaSavindi@users.noreply.github.com>
2022-07-05 00:36:53 +0530

    Add attachment upload and downlaod functionality

--- front-end/src/Pages/HandleLeavePage/Request.jsx
+++ front-end/src/Pages/HandleLeavePage/Request.jsx
@@ -1,69 +1,86 @@
-import { React, useState } from "react";
-import styles from "./Request.module.css";
-import { Modal } from "react-bootstrap";
-import Axios from "axios";
+import { React, useState } from 'react'
+import styles from './Request.module.css'
+import { Modal } from 'react-bootstrap'
+import Axios from 'axios'
+import { saveAs } from 'file-saver'
 
 const Request = (props) => {
- 
-  const [showA, setShowA] = useState(false);
-  const handleCloseA = () => setShowA(false);
-  const handleShowA = () => setShowA(true);
+  const [showA, setShowA] = useState(false)
+  const handleCloseA = () => setShowA(false)
+  const handleShowA = () => setShowA(true)
 
-  const [showR, setShowR] = useState(false);
-  const handleCloseR = () => setShowR(false);
-  const handleShowR = () => setShowR(true);
+  const [showR, setShowR] = useState(false)
+  const handleCloseR = () => setShowR(false)
+  const handleShowR = () => setShowR(true)
 
   const handleApprove = async (e) => {
-    e.preventDefault();
-    const value = e.target.name;
+    e.preventDefault()
+    const value = e.target.name
     //console.log(e.target.name);
-    const data = { leave_request_id: value };
-    await Axios.post("http://localhost:3001/api/supervisor/approve", data).then(
+    const data = { leave_request_id: value }
+    await Axios.post('http://localhost:3001/api/supervisor/approve', data).then(
       (res) => {
         if (!res.data.success) {
-          alert("Error occured !!");
+          alert('Error occured !!')
         } else {
-          handleCloseA();
-          window.location.reload(false);
+          handleCloseA()
+          window.location.reload(false)
         }
-      }
-    );
-  };
+      },
+    )
+  }
 
   const handleReject = async (e) => {
-    e.preventDefault();
-    const value = e.target.name;
-    const data = { leave_request_id: value };
+    e.preventDefault()
+    const value = e.target.name
+    const data = { leave_request_id: value }
     //console.log(e.target.name);
-    await Axios.post("http://localhost:3001/api/supervisor/reject", data).then(
+    await Axios.post('http://localhost:3001/api/supervisor/reject', data).then(
       (res) => {
         if (!res.data.success) {
-          alert("Error occured !!");
+          alert('Error occured !!')
         } else {
-          handleCloseA();
-          window.location.reload(false);
+          handleCloseA()
+          window.location.reload(false)
         }
-      }
-    );
-  };
+      },
+    )
+  }
 
   return (
     <div
-      className={`${styles["row"]} row border border-3 border-top border-light`}
+      className={`${styles['row']} row border border-3 border-top border-light`}
     >
       <div className="col text-center ">
         <div>
-          <img src={props.dp} alt="prof-pic" className={`${styles["dp"]}`} />
-          {props.cur.first_name + " " + props.cur.last_name}
+          <img src={props.dp} alt="prof-pic" className={`${styles['dp']}`} />
+          {props.cur.first_name + ' ' + props.cur.last_name}
         </div>
       </div>
       <div className="col text-center">{props.cur.type}</div>
       <div className="col text-center">
-        {props.cur.leave_begin + " - " + props.cur.leave_end}
+        {props.cur.leave_begin + ' - ' + props.cur.leave_end}
       </div>
-
       <div className="col-3 text-center">{props.cur.reason}</div>
-       <div className="col-1 text-center">{props.cur.attachment}</div>        {/*  TODO: attchement downlaod */}
+      <div className="col-1 text-center">
+        {props.cur.attachment ? (
+          <button
+            className="btn btn-primary btn-outline"
+            onClick={() => {
+              saveAs(
+                `http://localhost:3001/attachments/${props.cur.attachment}`,
+                'attachment.pdf',
+              )
+            }}
+          >
+            {' '}
+            Download
+          </button>
+        ) : (
+          <h6>No attachment available</h6>
+        )}
+      </div>{' '}
+      {/*  TODO: attchement downlaod */}
       <div className="col text-center">
         <div className="btn-group" role="group">
           <button
@@ -82,7 +99,6 @@ const Request = (props) => {
           </button>
         </div>
       </div>
-
       <Modal show={showA} onHide={handleCloseA} centered>
         <Modal.Header closeButton>
           <Modal.Title>Request Approval - {props.cur.first_name}</Modal.Title>
@@ -124,7 +140,6 @@ const Request = (props) => {
           </Modal.Footer>
         </form>
       </Modal>
-
       <Modal show={showR} onHide={handleCloseR} centered>
         <Modal.Header closeButton>
           <Modal.Title>Request Rejection - {props.cur.first_name}</Modal.Title>
@@ -167,7 +182,7 @@ const Request = (props) => {
         </form>
       </Modal>
     </div>
-  );
-};
+  )
+}
 
-export default Request;
+export default Request
