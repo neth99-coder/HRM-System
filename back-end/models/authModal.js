3f3dd51da452927f4db334f29a6009d31c798d7d
Nethmi Jayakody <83853442+neth99-coder@users.noreply.github.com>
2022-07-04 19:46:14 +0530

    not finish

--- back-end/models/authModal.js
+++ back-end/models/authModal.js
@@ -1,6 +1,7 @@
 const { json } = require("express");
 const db = require("../config/db");
 const bcrypt = require("bcrypt");
+const JWT = require("jsonwebtoken");
 
 //Signup employees
 function signupUser(obj) {
@@ -45,5 +46,48 @@ function signupUser(obj) {
         });
     });
 }
+
+// function login(employee){
+//     const { empId, password} = employee;
+//     return new Promise((resolve, reject) => {
+//         var sql = "SELECT emp_id, password FROM login";
+
+//         db.query(sql,(err, result) => {
+//             isUserIn = result.find((element) => {
+//                 return element.emp_id === empId;
+//               });
+//               if (isUserIn === undefined) {
+//                 return reject("User Not found");
+//               } 
+//               if (isUserIn.emp_id) {
+//                 let qry = "SELECT password,type_id FROM employee NATURAL JOIN login WHERE emp_id = ?";
+//                 db.query(qry, [empId], (err, result) => {
+//                   //   bcrypt.hash(password, saltRounds, function (err, hash) {
+//                   bcrypt.compare(password, result[0].password, function (err, result) {
+//                     if (result) {
+//                       const token = JWT.sign(
+//                         { emp_id, type, ID: isUserIn.ID },
+//                         process.env.ACCESS_TOKEN_SECRET,
+//                         {
+//                           expiresIn: "2d",
+//                         }
+//                       );
+//                       // const user = JWT.verify(token, process.env.ACCESS_TOKEN_SECRET);
+//                       // console.log(user);
+//                       return resolve(token);
+//                     } else {
+//                       return reject(err);
+//                     }
+//                   });
+//                   //   });   
+//                 });
+//               }              
+              
+//         });
+//     });
+// }
+
+
+
 
 module.exports = {signupUser};
